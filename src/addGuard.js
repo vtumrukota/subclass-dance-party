5cb9e7803df4e8c5437e43a70cef4a8284baf2fb
vtumrukota <vtumrukota@gmail.com>
2015-03-30 19:19:14 -0700

    Created Dancer class files

---
+++ src/addGuard.js
@@ -0,0 +1,26 @@
+var addGuard = function(top, left, timeBetweenSteps){
+  // var blinkyDancer = (top, left, timeBetweenSteps);
+
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="guard"></span>');
+
+
+};
+
+addGuard.prototype = Object.create(makeDancer.prototype);
+addGuard.prototype.constructor = addGuard;
+
+addGuard.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+    makeDancer.prototype.step.call(this);
+
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+
+    // blinkyDancer.$node.toggle();
+
+    // this.$node.toggle().call(this);
+    this.$node.toggle();
+};
+
